ae514f0ae5b1a3be5fccbb6caf43e330957c99c6
Howtoautomate.in.th <mart@howtoautomate.in.th>
2019-04-23 18:06:57 +0700

    Simple OOP concept in TypeScript

---
+++ oop/oop.js
@@ -0,0 +1,43 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var cat;
+cat = {
+    kind: 'mammal',
+    weight: 10,
+};
+// Class - Inherit
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log("Animal moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog.prototype.bark = function () {
+        console.log('Woof! Woof!');
+    };
+    return Dog;
+}(Animal));
+var dog = new Dog();
+dog.bark();
+dog.move(10);
+dog.bark();
