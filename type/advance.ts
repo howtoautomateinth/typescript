d95b63607abd9b299d40b27c65465c519fe483f4
Howtoautomateinth <mart@howtoautomate.in.th>
2019-09-10 13:48:19 +0700

    Added example of function type

--- type/advance.ts
+++ type/advance.ts
@@ -12,13 +12,22 @@ function getData<T>(data: string | T[]): string | T[] {
   return data;
 }
 
+type ExampleFuncType = (name: string) => string;
+function testFuncType(wordFuncType: ExampleFuncType): void {
+  console.log(wordFuncType("HTA Web"));
+}
+
 function main() {
   getWord("howtoautomate.in.th");
   getWord(["my", "array", "words"]);
   const exampleWorld: string | string[] = getData("example");
   const numbersOrder: string | number[] = getData([1, 2, 3, 4, 5]);
-  console.log(exampleWorld);
-  console.log(numbersOrder);
+  console.log(exampleWorld, numbersOrder);
+
+  let funcType: ExampleFuncType = (value: string) => {
+    return `this is your value ${value}`;
+  };
+  testFuncType(funcType);
 }
 
 main();
